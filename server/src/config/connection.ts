c6108ea38aa33e2224f7fecef70863fb84554451
jutalexa2024 <jajustinalexander@gmail.com>
2025-02-03 19:29:18 -0600

    Vite update

--- server/src/config/connection.ts
+++ server/src/config/connection.ts
@@ -4,17 +4,20 @@ dotenv.config();
 import { Sequelize } from 'sequelize';
 
 // TODO: Create and export the Sequelize connection to the database
-const sequelize = new Sequelize("postgres://postgres:postgres@localhost/eventure_db",
-{
-  dialect:'postgres'
-})
-  // ? new Sequelize(process.env.DB_URL)
-  // : new Sequelize(process.env.DB_NAME || '', process.env.DB_USER || '',process.env.DB_PASSWORD, {
-  //     host: 'localhost',
-  //     dialect: 'postgres',
-  //     dialectOptions: {
-  //       decimalNumbers: true,
-  //     },
-  //   });
+const sequelize = process.env.DB_URL
+  ? new Sequelize(process.env.DB_URL)
+  : new Sequelize(
+      process.env.DB_NAME || '',
+      process.env.DB_USER || '',
+      process.env.DB_PASSWORD,
+      {
+        host: 'localhost',
+        dialect: 'postgres',
+        dialectOptions: {
+          decimalNumbers: true,
+        },
+      }
+    );
+
 
 export default sequelize;
