d375f5e221360fd96278d503e40f6f85e059e1c9
jbarry89 <joelbarry8907@hotmail.com>
2025-02-04 20:56:08 -0500

    Added initial route path code

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,43 +2,33 @@ import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
 
-import App from './App.tsx';
 import ErrorPage from './pages/ErrorPage.tsx';
-import MainPage from './pages/MainPage.tsx';
-import VolunteerPage from './pages/VolunteerPage.tsx';
-import VolunteerForm from './pages/VolunteerForm.tsx';
-import EditVolunteer from './pages/EditVolunteer.tsx';
-import EditWork from './pages/EditWork.tsx';
+// import MainPage from './pages/MainPage.tsx';
+// import VolunteerPage from './pages/VolunteerPage.tsx';
+// import VolunteerForm from './pages/VolunteerForm.tsx';
+// import EditVolunteer from './pages/EditVolunteer.tsx';
+// import EditWork from './pages/EditWork.tsx';
+import LoginPage from './pages/LoginPage.tsx';
+// import SignUP from './pages/SignUPpage.tsx';
 
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <App />,
+    element: <LoginPage />,
     errorElement: <ErrorPage />,
-    children: [
-      {
-        index: true,
-        element: <MainPage />
-      }, 
-      {
-        path: '/show-volunteers',
-        element: <VolunteerPage />
-      },
-      {
-        path: '/new-volunteer',
-        element: <VolunteerForm />
-      },
-      {
-        path: '/edit-volunteer',
-        element: <EditVolunteer />
-      },
-      {
-        path: '/edit-work',
-        element: <EditWork />
-      }
-    ]
-  }
+    // children: [
+    //   {
+    //     index: true,
+    //     element: <Login />
+    //   }, 
+    //   {
+    //     path: '/signup',
+    //     element: <SignUP />
+    //   }
+      
+    // ]
+  },
 ]);
 
 const rootElement = document.getElementById('root');
